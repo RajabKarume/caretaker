febc67bd09b4b05697e9ad267226d83126ea27e1
Rajab Karume <rkarume@gmail.com>
2023-05-10 18:16:48 +0300

    ✨ created login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import SignIn from './libs/pages/login/SignIn/SignIn';
+import LoginPage from './libs/pages/login/LoginPage';
 
 function App() {
   return (
     <div>
-      <SignIn />
+      <LoginPage />
     </div>
   );
 }
---
+++ src/libs/pages/login/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './LoginPage.css'
+import SignIn from "./SignIn/SignIn";
+
+function LoginPage(){
+
+    return(
+        <div className="loginpage-div" >
+            <SignIn />
+        </div>
+    )
+}
+
+export default LoginPage
